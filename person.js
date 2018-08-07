90b2b5aae230a36c36ffd41e41defb1ebb0f4407
Pooja <pooja.kaul2015@gmail.com>
2018-08-07 17:35:17 +0200

    My first push to git

---
+++ person.js
@@ -0,0 +1,14 @@
+var person = { firstName: 'Rembert', lastName : 'Boom', Age: 34,place:'Amsterdam'}
+
+console.log('Hello,I am', person.firstName ,person.lastName);
+
+var student ={Name :'Michael', age: 22,
+
+months: function()
+{
+    return student.age*12;
+}
+
+}
+
+console.log('I am', student.Name,'and I am',student.age,'or' ,student.months(),'months old');
